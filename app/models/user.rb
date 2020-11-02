5b173c64edeca2a48a2e4de7b6f118e1260d587b
Mtwod <maximewong@hotmail.com>
2020-11-02 15:58:08 +0100

    added mail functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
     presence: true,
     uniqueness: true,
     format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "in wrong format" }
+  
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 
   has_many :admin_events, foreign_key: "event_admin_id", class_name: "Event", dependent: :destroy
   has_many :attendances, dependent: :destroy
