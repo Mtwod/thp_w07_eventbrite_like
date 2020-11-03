3fb78ca86c821748f3b503cbbd60008cd5f3b64f
Mtwod <maximewong@hotmail.com>
2020-11-03 18:26:40 +0100

    added User show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  
   def show
+    @user = User.find(params[:id])
+    unless is_author?(@user)
+      flash[:danger] = "You are not the owner of this profile !"
+      redirect_back(fallback_location: root_path)
+    end
+    @admin_events = current_user.admin_events
   end
 
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,6 @@ class User < ApplicationRecord
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
+
+  
 end
