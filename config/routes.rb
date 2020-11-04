03d35388acabc3cb574eec9e3746875d1b14170d
Mtwod <maximewong@hotmail.com>
2020-11-04 15:42:32 +0100

    Added gem 'stripe' and generated Controller Attendances

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   root 'events#index'
   resources :users
   resources :events
+  resources :attendances
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
